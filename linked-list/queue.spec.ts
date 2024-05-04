ae0701d3e1339ef4f1abf9437bbd3ca8f6d316de
Danny Franca <me@dannyfranca.com>
2024-05-04 02:32:30 +0100

    feat: add queue

---
+++ linked-list/queue.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "bun:test";
+import { Queue } from "./queue";
+
+test("queue", function () {
+  const list = new Queue<number>();
+
+  list.enqueue(5);
+  list.enqueue(7);
+  list.enqueue(9);
+
+  expect(list.dequeue()).toEqual(5);
+  expect(list.length).toEqual(2);
+
+  // this must be wrong..?
+  debugger;
+
+  // i hate using debuggers
+  list.enqueue(11);
+  debugger;
+  expect(list.dequeue()).toEqual(7);
+  expect(list.dequeue()).toEqual(9);
+  expect(list.peek()).toEqual(11);
+  expect(list.dequeue()).toEqual(11);
+  expect(list.dequeue()).toBeUndefined();
+  expect(list.length).toEqual(0);
+
+  // just wanted to make sure that I could not blow up myself when i remove
+  // everything
+  list.enqueue(69);
+  expect(list.peek()).toEqual(69);
+  expect(list.length).toEqual(1);
+});
---
+++ linked-list/queue.ts
@@ -0,0 +1,44 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
+export class Queue<T> {
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
+
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
+
+  enqueue(item: T): void {
+    const node = { value: item };
+    this.length++;
+    if (!this.tail) {
+      this.tail = this.head = node;
+      return;
+    }
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  dequeue(): T | undefined {
+    if (!this.head) return;
+    this.length--;
+    const prev = this.head;
+    this.head = this.head.next;
+
+    prev.next = undefined;
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+
+    return prev.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+}
