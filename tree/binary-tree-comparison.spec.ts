127ac367d62bf19346ed976fbfa59b729a5f3f2f
Danny Franca <me@dannyfranca.com>
2024-05-05 10:06:02 +0100

    feat: add recursive version for binary tree comparison

--- tree/binary-tree-comparison.spec.ts
+++ tree/binary-tree-comparison.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from "bun:test";
-import { compare } from "./binary-tree-comparison";
+import { compare, compareRecursive } from "./binary-tree-comparison";
 import { tree, tree2 } from "./tree-sample";
 
 test("Compare Binary Trees", function () {
   expect(compare(tree, tree)).toEqual(true);
   expect(compare(tree, tree2)).toEqual(false);
+  expect(compareRecursive(tree, tree)).toEqual(true);
+  expect(compareRecursive(tree, tree2)).toEqual(false);
 });
--- tree/binary-tree-comparison.ts
+++ tree/binary-tree-comparison.ts
@@ -1,12 +1,32 @@
 import type { BinaryNode } from "./type";
 
 export function compare<T>(
-  a: BinaryNode<T> | null | undefined,
-  b: BinaryNode<T> | null | undefined
+  a: BinaryNode<T> | null,
+  b: BinaryNode<T> | null
 ): boolean {
   if (!a && !a) return true;
   if (!a || !b) return false;
   if (a.value !== b.value) return false;
 
   return compare(a.left, b.left) && compare(a.right, b.right);
+}
+
+export function compareRecursive<T>(
+  bta: BinaryNode<T> | null,
+  btb: BinaryNode<T> | null
+): boolean {
+  const stack: [
+    BinaryNode<T> | null | undefined,
+    BinaryNode<T> | null | undefined
+  ][] = [[bta, btb]];
+
+  while (stack.length) {
+    const [a, b] = stack.pop()!;
+    if (!a && !b) continue;
+    if (!a || !b) return false;
+    if (a.value !== b.value) return false;
+    stack.push([a.left, b.left], [a.right, b.right]);
+  }
+
+  return true;
 }
