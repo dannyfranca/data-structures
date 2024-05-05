4017b3f5dee8106220df90aada8ca64e58c71f23
Danny Franca <me@dannyfranca.com>
2024-05-05 12:29:08 +0100

    feat: implement depth-first search recursive

--- tree/depth-first-search.spec.ts
+++ tree/depth-first-search.spec.ts
@@ -1,9 +1,15 @@
 import { test, expect } from "bun:test";
-import { depthFirstSearch } from "./depth-first-search";
+import {
+  depthFirstSearch,
+  depthFirstSearchRecursive,
+} from "./depth-first-search";
 import { tree } from "./tree-sample";
 
 test("DFS on BST", function () {
   expect(depthFirstSearch(tree, 45)).toEqual(true);
   expect(depthFirstSearch(tree, 7)).toEqual(true);
   expect(depthFirstSearch(tree, 69)).toEqual(false);
+  expect(depthFirstSearchRecursive(tree, 45)).toEqual(true);
+  expect(depthFirstSearchRecursive(tree, 7)).toEqual(true);
+  expect(depthFirstSearchRecursive(tree, 69)).toEqual(false);
 });
--- tree/depth-first-search.ts
+++ tree/depth-first-search.ts
@@ -10,3 +10,23 @@ function search<T>(node: BinaryNode<T> | null, needle: T): boolean {
   if (needle <= node.value) return search(node.left, needle);
   return search(node.right, needle);
 }
+
+export function depthFirstSearchRecursive<T>(
+  node: BinaryNode<T>,
+  needle: T
+): boolean {
+  const stack = [node];
+  let curr: BinaryNode<T>;
+
+  while (stack.length) {
+    curr = stack.pop()!;
+    if (curr.value === needle) return true;
+    if (needle <= curr.value && curr.left) {
+      stack.push(curr.left);
+      continue;
+    }
+    if (needle > curr.value && curr.right) stack.push(curr.right);
+  }
+
+  return false;
+}
