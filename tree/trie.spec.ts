3eb8a4fce0b68c2966c78f4fd2709b2ea79a951e
Danny Franca <me@dannyfranca.com>
2024-05-10 07:29:19 +0100

    feat: add trie

---
+++ tree/trie.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "bun:test";
+import { Trie } from "./trie";
+
+test("Trie", function () {
+  const trie = new Trie();
+
+  trie.insert("apple");
+  trie.insert("app");
+  trie.insert("banana");
+
+  expect(trie.search("apple")).toBe(true);
+  expect(trie.search("app")).toBe(true);
+  expect(trie.search("banana")).toBe(true);
+  expect(trie.search("grape")).toBe(false);
+
+  expect(trie.startsWith("ban")).toBe(true);
+  expect(trie.startsWith("app")).toBe(true);
+  expect(trie.startsWith("orange")).toBe(false);
+});
